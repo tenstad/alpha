096fce5764eef96a4bca860d02fdd84d89188b7a
Amund Tenstad <github@amund.io>
2025-04-10 12:03:45 +0200

    wip: cranelift ir

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use clap::Parser;
 use eval::Eval;
 use parser::AlphaParser;
 use std::fs;
+use trans::Translator;
 
 mod ast;
 mod eval;
 mod parser;
+mod trans;
 
 #[macro_use]
 extern crate lazy_static;
@@ -24,7 +26,10 @@ fn main() {
     let program = fs::read_to_string(args.file).unwrap();
 
     match AlphaParser::parse_source(program.as_str(), args.debug) {
-        Ok(ast) => Eval::default().run(&ast),
+        Ok(ast) => {
+            Eval::default().run(&ast);
+            Translator::translate(&ast);
+        }
         Err(e) => println!("{}", e),
     }
 }
